1fa6ffbe31a49cc003e6d18bd0d6f20c723597f6
sscoach <developer.kss@gmail.com>
2025-04-27 23:05:07 +0900

    refactoring

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import pygame
 
-from game_objects.alien import Alien
-from game_objects.fighter import Fighter
+from objects.alien import Alien
+from objects.fighter import Fighter
+
+from constants import *
 
 print("Startup")
 pygame.init()
 pygame.key.set_repeat(500, 500)
-surface = pygame.display.set_mode((640, 480))
+surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
 fighter = Fighter()
 
 aliens = []
-for y in range(2): # y: 0, 1
-    for x in range(3): # x: 0, 1, 2
+for y in range(2):  # y: 0, 1
+    for x in range(3):  # x: 0, 1, 2
         alien = Alien()
         aliens.append(alien)
         alien.x = 70 + 50 * x
@@ -37,4 +39,5 @@ while True:
         alien.draw(surface)
 
     pygame.display.update()
-    clock.tick(30)
+    clock.tick(FPS)
+
--- objects/fighter.py
+++ objects/fighter.py
@@ -1,5 +1,5 @@
 import pygame
-
+from constants import *
 
 class Fighter:
     def __init__(self):
@@ -8,8 +8,8 @@ class Fighter:
             image,
             (image.get_width() * 2, image.get_height() * 2)
         )
-        self.x = 640 / 2 - self.scale_up_image.get_width() / 2
-        self.y = 480 - self.scale_up_image.get_height() - 10
+        self.x = SCREEN_WIDTH / 2 - self.scale_up_image.get_width() / 2
+        self.y = SCREEN_HEIGHT - self.scale_up_image.get_height() - 10
 
     def draw(self, surface):
         surface.blit(self.scale_up_image, (self.x, self.y))
