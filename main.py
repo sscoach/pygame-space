fd5f75df25907068afe29cbb775c3451568d277b
sscoach <developer.kss@gmail.com>
2025-05-05 11:20:13 +0900

    shoot the beam

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from objects.alien import Alien
 from objects.fighter import Fighter
+from objects.beam import Beam
 
 from constants import *
 
@@ -12,6 +13,7 @@ surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
 fighter = Fighter()
+beam = None
 
 aliens = []
 for y in range(2):  # y: 0, 1
@@ -36,6 +38,10 @@ while True:
                 fighter.direction_x = -1
             if event.key == pygame.K_RIGHT:
                 fighter.direction_x = +1
+            if event.key == pygame.K_SPACE:
+                if beam is None:
+                    beam = Beam(fighter.x + fighter.image.get_width()/2, fighter.y)
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 fighter.direction_x = 0
@@ -44,6 +50,10 @@ while True:
 
     delta_seconds = clock.tick(FPS) / 1000
     fighter.update(delta_seconds)
+    if beam:
+        beam.update(delta_seconds)
+        if beam.y < 0:
+            beam = None
 
     for alien in aliens:
         alien.update(delta_seconds)
@@ -58,6 +68,8 @@ while True:
     # print("Render")
     surface.fill((0, 0, 0))
     fighter.draw(surface)
+    if beam:
+        beam.draw(surface)
 
     for alien in aliens:
         alien.draw(surface)
