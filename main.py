d209ab7b1dd25d2cfeb63e674e4c4f94c9a2a02f
sscoach <developer.kss@gmail.com>
2025-05-10 12:10:36 +0900

    game_scene

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from objects.alien import Alien
 from objects.fighter import Fighter
 from objects.beam import Beam
 from objects.explosion import Explosion
-
+from scenes.game_scene import GameScene
 from constants import *
 
 print("Startup")
@@ -13,26 +13,8 @@ pygame.key.set_repeat(500, 300)
 surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
-fighter = Fighter()
-beams = []
-
-aliens = []
-for y in range(2):  # y: 0, 1
-    for x in range(3):  # x: 0, 1, 2
-        alien = Alien()
-        aliens.append(alien)
-        alien.x = 70 + 50 * x
-        alien.y = 100 + 70 * y
-bombs = []
-
-explosions = []
-
-shoot_sound = pygame.mixer.Sound("assets/sounds/shoot.wav")
-invaderkilled_sound = pygame.mixer.Sound("assets/sounds/invaderkilled.wav")
-explosion_sound = pygame.mixer.Sound("assets/sounds/explosion.wav")
-shoot_sound.set_volume(0.05)
-invaderkilled_sound.set_volume(0.05)
-explosion_sound.set_volume(0.05)
+scene = GameScene()
+scene.on_begin()
 
 while True:
 
@@ -45,89 +27,17 @@ while True:
             exit()
             break
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                fighter.direction_x = -1
-            if event.key == pygame.K_RIGHT:
-                fighter.direction_x = +1
-            if event.key == pygame.K_SPACE:
-                if len(beams) < 2:
-                    beam = Beam(fighter.x + fighter.image.get_width()/2, fighter.y)
-                    beams.append(beam)
-                    shoot_sound.play()
+            scene.on_key_down(event.key)
 
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                fighter.direction_x = 0
-            if event.key == pygame.K_RIGHT:
-                fighter.direction_x = 0
+            scene.on_key_up(event.key)
 
     delta_seconds = clock.tick(FPS) / 1000
-    fighter.update(delta_seconds)
-    for beam in beams:
-        beam.update(delta_seconds)
-        if beam.y < 0:
-            beams.remove(beam)
-        else:
-            alien = beam.check_collision(aliens)
-            if alien:
-                explosions.append(Explosion(alien.rect))
-                aliens.remove(alien)
-                beams.remove(beam)
-                invaderkilled_sound.play()
-
-    for alien in aliens:
-        alien.update(delta_seconds)
-
-        bomb = alien.shoot()
-        if bomb:
-            bombs.append(bomb)
-
-        if alien.check_collision([fighter]):
-            explosions.append(Explosion(fighter.rect))
-            explosions.append(Explosion(alien.rect))
-            aliens.remove(alien)
-            explosion_sound.play()
-            print("Game Over")
-            break
-
-    for bomb in bombs:
-        bomb.update(delta_seconds)
-        if SCREEN_HEIGHT < bomb.y:
-            bombs.remove(bomb)
-        else:
-            if bomb.check_collision([fighter]):
-                explosions.append(Explosion(fighter.rect))
-                bombs.remove(bomb)
-                explosion_sound.play()
-                print("Game Over")
-                break
-
-    for explosion in explosions:
-        explosion.update(delta_seconds)
-        if explosion.is_finished():
-            explosions.remove(explosion)
-
-    if Alien.should_change_direction:
-        Alien.should_change_direction = False
-
-        for alien in aliens:
-            alien.direction_x *= -1
-            alien.move(0, 50)
+    scene.on_update(delta_seconds)
 
     # print("Render")
     surface.fill((0, 0, 0))
-    fighter.draw(surface)
-    for beam in beams:
-        beam.draw(surface)
-
-    for alien in aliens:
-        alien.draw(surface)
-
-    for bomb in bombs:
-        bomb.draw(surface)
-
-    for explosion in explosions:
-        explosion.draw(surface)
+    scene.on_render(surface)
 
     pygame.display.update()
 
---
+++ scenes/game_scene.py
@@ -0,0 +1,123 @@
+import pygame
+
+from objects.alien import Alien
+from objects.fighter import Fighter
+from objects.beam import Beam
+from objects.explosion import Explosion
+
+from constants import *
+from scenes.base_scene import BaseScene
+
+
+class GameScene(BaseScene):
+    def __init__(self):
+        self.fighter = Fighter()
+        self.beams = []
+
+        self.aliens = []
+        for y in range(2):  # y: 0, 1
+            for x in range(3):  # x: 0, 1, 2
+                alien = Alien()
+                self.aliens.append(alien)
+                alien.x = 70 + 50 * x
+                alien.y = 100 + 70 * y
+        self.bombs = []
+
+        self.explosions = []
+
+        self.shoot_sound = pygame.mixer.Sound("assets/sounds/shoot.wav")
+        self.invaderkilled_sound = pygame.mixer.Sound("assets/sounds/invaderkilled.wav")
+        self.explosion_sound = pygame.mixer.Sound("assets/sounds/explosion.wav")
+        self.shoot_sound.set_volume(0.05)
+        self.invaderkilled_sound.set_volume(0.05)
+        self.explosion_sound.set_volume(0.05)
+
+    def on_begin(self):
+        pass
+
+    def on_end(self):
+        pass
+
+    def on_key_down(self, key):
+        if key == pygame.K_LEFT:
+            self.fighter.direction_x = -1
+        if key == pygame.K_RIGHT:
+            self.fighter.direction_x = +1
+        if key == pygame.K_SPACE:
+            if len(self.beams) < 2:
+                beam = Beam(self.fighter.x + self.fighter.image.get_width() / 2, self.fighter.y)
+                self.beams.append(beam)
+                self.shoot_sound.play()
+
+    def on_key_up(self, key):
+        if key == pygame.K_LEFT:
+            self.fighter.direction_x = 0
+        if key == pygame.K_RIGHT:
+            self.fighter.direction_x = 0
+
+    def on_update(self, delta_seconds):
+        self.fighter.update(delta_seconds)
+        for beam in self.beams:
+            beam.update(delta_seconds)
+            if beam.y < 0:
+                self.beams.remove(beam)
+            else:
+                alien = beam.check_collision(self.aliens)
+                if alien:
+                    self.explosions.append(Explosion(alien.rect))
+                    self.aliens.remove(alien)
+                    self.beams.remove(beam)
+                    self.invaderkilled_sound.play()
+
+        for alien in self.aliens:
+            alien.update(delta_seconds)
+
+            bomb = alien.shoot()
+            if bomb:
+                self.bombs.append(bomb)
+
+            if alien.check_collision([self.fighter]):
+                self.explosions.append(Explosion(self.fighter.rect))
+                self.explosions.append(Explosion(alien.rect))
+                self.aliens.remove(alien)
+                self.explosion_sound.play()
+                print("Game Over")
+                break
+
+        for bomb in self.bombs:
+            bomb.update(delta_seconds)
+            if SCREEN_HEIGHT < bomb.y:
+                self.bombs.remove(bomb)
+            else:
+                if bomb.check_collision([self.fighter]):
+                    self.explosions.append(Explosion(self.fighter.rect))
+                    self.bombs.remove(bomb)
+                    self.explosion_sound.play()
+                    print("Game Over")
+                    break
+
+        for explosion in self.explosions:
+            explosion.update(delta_seconds)
+            if explosion.is_finished():
+                self.explosions.remove(explosion)
+
+        if Alien.should_change_direction:
+            Alien.should_change_direction = False
+
+            for alien in self.aliens:
+                alien.direction_x *= -1
+                alien.move(0, 50)
+
+    def on_render(self, surface):
+        self.fighter.draw(surface)
+        for beam in self.beams:
+            beam.draw(surface)
+
+        for alien in self.aliens:
+            alien.draw(surface)
+
+        for bomb in self.bombs:
+            bomb.draw(surface)
+
+        for explosion in self.explosions:
+            explosion.draw(surface)
