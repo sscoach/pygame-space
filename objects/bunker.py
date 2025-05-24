8192f683c84078544a1c3f56bae15fc4414ef840
sscoach <developer.kss@gmail.com>
2025-05-24 17:51:37 +0900

    position bunkers

---
+++ objects/bunker.py
@@ -0,0 +1,8 @@
+from objects.object import Object
+
+class Bunker(Object):
+    def __init__(self, x, y):
+        super().__init__("assets/images/bunker.png")
+
+        self.x = x
+        self.y = y
